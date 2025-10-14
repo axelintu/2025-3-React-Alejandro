9fba86c45a9146aed3adfe606d52c642f7af3b28
axelintu <darklintumon@gmail.com>
2025-10-13 19:42:33 -0600

    Added controlled input to TodoForm

--- 02.Doing/to-do-list/src/components/TodoForm/TodoForm.jsx
+++ 02.Doing/to-do-list/src/components/TodoForm/TodoForm.jsx
@@ -1,15 +1,48 @@
 import { PropTypes } from 'prop-types';
 import './TodoForm.css';
+import { useState } from 'react';
 
 function TodoForm ({onAdd}) {
+  const [newTask, setNewTask] = useState('');
+  const [emptyTask, setEmptyTask] = useState(false);
+  function validateNewTask (newTask) {
+    if (newTask==='') {
+      console.log('vacio', newTask, emptyTask);
+      setEmptyTask(true);
+    } else {
+      setEmptyTask(false);
+    }
+  }
   return (
     <div className="new-task">
+      <div>
+        NewTask: '{newTask}'
+      </div>
       <input 
-      type="text" 
-      name="newTaskInput" 
-      id="newTaskInput" 
-      className='new-task-input' />
-      <button type="button"><span>Agregar nueva tarea</span></button>
+        type="text" 
+        name="newTaskInput" 
+        id="newTaskInput" 
+        className='new-task-input'
+        value={newTask} 
+        onChange={(e) => {
+          console.log(e);
+          
+          setNewTask(e.target.value)
+          }
+        }/>
+      <button
+        disabled={newTask===''}
+        type="button" 
+        onClick={
+        () => {
+          validateNewTask(newTask);
+          // console.log(newTask)
+        }}>
+          <span>Agregar nueva tarea</span>
+      </button>
+      {emptyTask && <div>
+        ⚠️ El título no puede estar vacío
+      </div>}
     </div>
   )
 }
